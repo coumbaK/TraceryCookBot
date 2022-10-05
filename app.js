8907da24acd94a1ec5ca2b6afdee5259f61cc215
Glitch (glitch-hello-website) <none>
2022-10-05 02:39:57 +0000

    🏤🍈 Checkpoint ./app.js:270926/9786 ./brushes.js:270926/359 ./index.html:270926/101

--- app.js
+++ app.js
@@ -4,12 +4,10 @@
  * Each swatch has code for when it starts and each frame after
  */
 
-
-
 window.addEventListener("load", function () {
-  console.log("LOADED")
-   
-   let activeTool = undefined
+  console.log("LOADED");
+
+  let activeTool = undefined;
   // Create a P5 canvas element, JS-style
   // https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
   const s = (p) => {
@@ -17,160 +15,174 @@ window.addEventListener("load", function () {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.colorMode(p.HSL, 360, 100, 100);
       p.ellipseMode(p.CENTER_RADIUS);
-      p.circle(0, 0, 100)
+      p.circle(0, 0, 100);
+    };
+
+    p.mouseDragged = function () {
+      if (activeTool.mouseDragged) activeTool.mouseDragged();
+    };
+
+    p.mousePressed = function () {
+      console.log("Drag");
+    };
+
+    p.mouseReleased = function () {
+      console.log("Release");
     };
 
     p.draw = function () {
-      p.background(150, 100, 50)
-      let t = p.millis() * 0.001;
-    
+      if (activeTool.draw) activeTool.draw();
     };
   };
-  
+
   function setBrush(brush) {
-    const DESC_EL = document.getElementById("brush-desc")
-  
-    activeTool = brush
-    console.log("Set active tool to ", brush.label, brush.description)
+    const DESC_EL = document.getElementById("brush-desc");
+    
+
+    activeTool = brush;
+    if (activeTool.setup) activeTool.setup();
+    console.log("Set active tool to ", brush.label, brush.description);
   }
-  
-  const CANVAS_EL = document.getElementById("canvas-holder")
-  CANVAS_EL.style.width = CANVAS_WIDTH + 'px';
-  CANVAS_EL.style.height = CANVAS_HEIGHT + 'px';
-  
-  let myp5 = new p5(s, CANVAS_EL);
-  
-  const BUTTON_HOLDER_EL = document.getElementById("buttons")
-  
-//   Set the initial brush and color values
-  setBrush(brushes[START_BRUSH_INDEX])
-  document.getElementById("color0").value = HSLToHex(...tool.color0)
-	document.getElementById("color1").value = HSLToHex(...tool.color1)
-  
-  brushes.forEach(brush => {
-    let button = document.createElement("button")
-    button.innerHTML = brush.label
-    
-    BUTTON_HOLDER_EL.appendChild(button)
-    
-    button.addEventListener("click", () => {
-      setBrush(brush)
+
+  const CANVAS_EL = document.getElementById("canvas-holder");
+  CANVAS_EL.style.width = CANVAS_WIDTH + "px";
+  CANVAS_EL.style.height = CANVAS_HEIGHT + "px";
+
+  p = new p5(s, CANVAS_EL);
+
+  const BUTTON_HOLDER_EL = document.getElementById("buttons");
+
+  //   Set the initial brush and color values
+  setBrush(brushes[START_BRUSH_INDEX]);
+  const COLOR0_EL = document.getElementById("color0")
+  const COLOR1_EL = document.getElementById("color1")
+  COLOR0_EL.value = HSLToHex(...color0);
+ COLOR1_EL.value = HSLToHex(...color1);
+   COLOR1_EL.addEventListener("change", function() {
+     console.log("Change color", this.value)
+     color1 = HexToHSL
     });
+  
 
-  })
+  brushes.forEach((brush) => {
+    let button = document.createElement("button");
+    button.innerHTML = brush.label;
+
+    BUTTON_HOLDER_EL.appendChild(button);
+
+    button.addEventListener("click", () => {
+      setBrush(brush);
+    });
+  });
 });
-
 
 //=========================================
 // Utility functions
 // Given a processing object, a pct around the circle, a radius, and an offset (optional)
-function getLoopingNoise({
-	p,
-	loopPct,
-	radius,
-	offset = 0
-}) {
-
+function getLoopingNoise({ p, loopPct, radius, offset = 0 }) {
   // This number should go from 0 to 1 every loopLength seconds
   // And PI*2 radians every loopLength seconds
 
-  let theta = 2 * Math.PI * loopPct
+  let theta = 2 * Math.PI * loopPct;
 
   // Place to sample the noise from
-  let x = radius * Math.cos(theta)
-  let y = radius * Math.sin(theta)
+  let x = radius * Math.cos(theta);
+  let y = radius * Math.sin(theta);
 
-  let noiseVal = p.noise(x + 100, y + 30, offset)
+  let noiseVal = p.noise(x + 100, y + 30, offset);
 
-  return noiseVal
+  return noiseVal;
 }
-
-
 
 // Hex to HSLA conversion adapted from
 // https://css-tricks.com/converting-color-spaces-in-javascript/
 // But now it resutn
 function hexToHSL(H) {
-	// Convert hex to RGB first
-	let r = 0, g = 0, b = 0;
-	if (H.length == 4) {
-		r = "0x" + H[1] + H[1];
-		g = "0x" + H[2] + H[2];
-		b = "0x" + H[3] + H[3];
-	} else if (H.length == 7) {
-		r = "0x" + H[1] + H[2];
-		g = "0x" + H[3] + H[4];
-		b = "0x" + H[5] + H[6];
-	}
-	// Then to HSL
-	r /= 255;
-	g /= 255;
-	b /= 255;
-	let cmin = Math.min(r,g,b),
-	cmax = Math.max(r,g,b),
-	delta = cmax - cmin,
-	h = 0,
-	s = 0,
-	l = 0;
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
 
-	if (delta == 0)
-		h = 0;
-	else if (cmax == r)
-		h = ((g - b) / delta) % 6;
-	else if (cmax == g)
-		h = (b - r) / delta + 2;
-	else
-		h = (r - g) / delta + 4;
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
 
-	h = Math.round(h * 60);
+  h = Math.round(h * 60);
 
-	if (h < 0)
-		h += 360;
+  if (h < 0) h += 360;
 
-	l = (cmax + cmin) / 2;
-	s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-	s = +(s * 100).toFixed(1);
-	l = +(l * 100).toFixed(1);
-	return [h, s, l]
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+  return [h, s, l];
 }
 
-function HSLToHex(h,s,l) {
+function HSLToHex(h, s, l) {
+  s /= 100;
+  l /= 100;
 
-	s /= 100;
-	l /= 100;
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
 
-	let c = (1 - Math.abs(2 * l - 1)) * s,
-	x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-	m = l - c/2,
-	r = 0,
-	g = 0, 
-	b = 0; 
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  // Having obtained RGB, convert channels to hex
+  r = Math.round((r + m) * 255).toString(16);
+  g = Math.round((g + m) * 255).toString(16);
+  b = Math.round((b + m) * 255).toString(16);
 
-	if (0 <= h && h < 60) {
-		r = c; g = x; b = 0;
-	} else if (60 <= h && h < 120) {
-		r = x; g = c; b = 0;
-	} else if (120 <= h && h < 180) {
-		r = 0; g = c; b = x;
-	} else if (180 <= h && h < 240) {
-		r = 0; g = x; b = c;
-	} else if (240 <= h && h < 300) {
-		r = x; g = 0; b = c;
-	} else if (300 <= h && h < 360) {
-		r = c; g = 0; b = x;
-	}
-	// Having obtained RGB, convert channels to hex
-	r = Math.round((r + m) * 255).toString(16);
-	g = Math.round((g + m) * 255).toString(16);
-	b = Math.round((b + m) * 255).toString(16);
+  // Prepend 0s, if necessary
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
 
-	// Prepend 0s, if necessary
-	if (r.length == 1)
-		r = "0" + r;
-	if (g.length == 1)
-		g = "0" + g;
-	if (b.length == 1)
-		b = "0" + b;
-
-	return "#" + r + g + b;
+  return "#" + r + g + b;
 }
--- brushes.js
+++ brushes.js
@@ -2,8 +2,9 @@ const CANVAS_WIDTH = 600
 const CANVAS_HEIGHT = 400
 
 // Current tool settings
-let color0 = []
-let color1 = []
+let p // Processing object, accessible from anywhere
+let color0 = [160, 100, 50]
+let color1 = [320, 100, 50]
 let size = 1
 
 // Change this to whichever brush you are working on
@@ -12,7 +13,14 @@ const START_BRUSH_INDEX = 0
 let brushes = [{
   label: "🖌",
   description: "basic paint brush",
-  draw(p, t) {
+  mouseDragged() {
+    console.log("Drag...")
+    let x = p.mouseX
+     let y = p.mouseY
+     console.log(x, y)
+     
     
-  }
+     p.fill(color0[0], color0[1], color0[2])
+     p.circle(x, y, 100)
+ }
 }]
