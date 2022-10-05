ae9a1d10b41364a448eb87e64c3e56e34058987c
Glitch (glitch-hello-website) <none>
2022-10-05 02:27:49 +0000

    💅⚽️ Checkpoint ./app.js:270926/2074 ./brushes.js:270926/165

--- brushes.js
+++ brushes.js
@@ -1,6 +1,14 @@
 const CANVAS_WIDTH = 600
 const CANVAS_HEIGHT = 400
 
+// Current tool settings
+let color0 = []
+let color1 = []
+let size = 1
+
+// Change this to whichever brush you are working on
+const START_BRUSH_INDEX = 0
+
 let brushes = [{
   label: "🖌",
   description: "basic paint brush",
