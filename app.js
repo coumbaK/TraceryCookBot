6c1dc1669e91dad7b97a3a7e26546db0ee4e64ef
Glitch (glitch-hello-website) <none>
2022-10-11 22:25:25 +0000

    😀🎷 Checkpoint ./app.js:270926/2702

--- app.js
+++ app.js
@@ -4,60 +4,42 @@
  * Each swatch has code for when it starts and each frame after
  */
 
-const CANVAS_WIDTH = 400
-const CANVAS_HEIGHT = 300
+let p;
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 300;
+
 window.addEventListener("load", function () {
   console.log("LOADED");
-  
-  let ps = new ParticleSystem()
+
+  let ps = new ParticleSystem();
 
   let activeTool = undefined;
   // Create a P5 canvas element, JS-style
   // https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
-  const s = (p) => {
+  const s = (p0) => {
+    p = p0;
     p.setup = function () {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.colorMode(p.HSL, 360, 100, 100);
       p.ellipseMode(p.RADIUS);
-      
     };
 
-
     p.draw = function () {
-      p.background(50)
-      
-       ps.update(p, p.deltaTime*.001)
-      
-      p.push()
-      p.translate(p.width/2, p.height/2)
+      p.background(50);
+
+      ps.update(p, p.deltaTime * 0.001);
+
+      p.push();
+      p.translate(p.width / 2, p.height / 2);
       // p.circle(0, 0, 100)
-      ps.draw(p)
-      p.pop()
+      ps.draw(p);
+      p.pop();
     };
   };
 
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   const CANVAS_EL = document.getElementById("canvas-holder");
   CANVAS_EL.style.width = CANVAS_WIDTH + "px";
   CANVAS_EL.style.height = CANVAS_HEIGHT + "px";
 
-  p = new p5(s, CANVAS_EL);
-
-
-
- 
+  new p5(s, CANVAS_EL);
 });
