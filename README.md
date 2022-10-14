bd2002de3dcdbf1fd27b9effdcf83e3dac8c7a44
Glitch (glitch-hello-website) <none>
2022-10-14 04:29:11 +0000

    🧀👑 Checkpoint ./p_boids.js:270926/1708 ./README.md:270926/80

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ particle stuff
 * Change the XSystem and XParticle class names to a new name
 * Add this new js file in `index.html`
 * Add this system's class name to the array at the top of app.js
+
+add this.draggable = true to make a particle draggable
--- p_boids.js
+++ p_boids.js
@@ -11,15 +11,40 @@ class BoidSystem extends ParticleSystem {
   constructor() {
     // Make what particle, and how many?
     // Try different numbers of particles
-    super(BoidParticle, 10);
+    super(BoidParticle, 40);
     
     this.flockCenter = new Vector2D()
+    this.flockVelocity = new Vector2D()
+  }
+  
+  beforeMove(p, dt) {
+//     Calculate the flock's center and average direction
+    
+    this.flockCenter.mult(0)
+    this.flockVelocity.mult(0)
+    this.particles.forEach(pt => {
+      this.flockCenter.add(pt.pos)
+      this.flockVelocity.add(pt.v)
+    })
+    this.flockVelocity.div(this.particles.length)
+    this.flockCenter.div(this.particles.length)
+  }
+  
+  
+  mousePressed(p) {
+    super.mousePressed(p)
+    if (!held) {
+      
+    }
   }
 
   draw(p) {
     // A little bit of trails!
     p.background(0, 0, 50, 0.5);
-   
+     
+    p.noFill()
+    p.stroke(100, 0, 100, .4)
+    p.circle(...this.flockCenter, 100)
 
     // The "super-class" draws the particles
     super.draw(p);
@@ -35,6 +60,8 @@ class BoidParticle extends Particle {
     // ps: the particle system this particle belongs to
     // index: of all the particles in that system, this one's index
     super(ps, index);
+    
+    this.draggable = true
 
     this.pos.setToRandom(0, p.width, 0, p.height);
     this.radius = 10
@@ -43,10 +70,20 @@ class BoidParticle extends Particle {
   }
 
   calculateForces(p, dt) {
-    
+     this.angle = this.v.angle
+    let center = new Vector2D(p.width/2, p.height/2)
+    // Add a border force
+    this.f.add(this.pos.getForceTowardsPoint(center, 1, {startRadius: 120, falloff: 1.2}))
   }
+  
+  // move(p, dt) {
+  //   super.move(p, dt)
+  //    this.pos.wrapX(0, p.width);
+  //   this.pos.wrapY(0, p.height);
+  // }
 
   draw(p, drawDebug = false) {
+    
     let t = p.millis() * 0.001;
   
     p.noStroke();
@@ -68,7 +105,7 @@ class BoidParticle extends Particle {
     
     
     if (drawDebug) {
-      this.pos.drawArrow(p, this.attractionForce, { m: 0.1 });
+      
     }
   }
 }
