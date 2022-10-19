6fec8bb25c2ce4e5ceae7185ebebe620dc19d869
Glitch (glitch-hello-website) <none>
2022-10-19 00:41:07 +0000

    🌒🍂 Checkpoint ./generators.js:270926/793

--- generators.js
+++ generators.js
@@ -23,13 +23,13 @@ const GENERATORS = {
       
       let size = dna[0]*60 + 40
       let aspectRatio = dna[1] + .6
-      let angle = dna[1] - .5
+      let angle = dna[2] - .5
       
       // How about a little bounce at least?
       let bounce = Math.abs(Math.sin(t*3))
       aspectRatio += .2 + -.5*bounce
-      let stickiness = .3
-      let jumpHeight = 60
+      let stickiness = .2
+      let jumpHeight = 40
       y -= Math.max(0, jumpHeight*(Math.abs(Math.sin(t*3 + .2)) - stickiness))
       angle = p.lerp(0, angle, bounce)
       
@@ -57,31 +57,28 @@ const GENERATORS = {
       let eyeHeight = h*.5
       let eyeSize = 10
       
-      function drawEye() {
-        
-          p.fill(0)
+      function drawEye({x, y, eyeSize, innerColor, outerColor = [0,], blink}) {
+          p.push()
+        p.translate(x, y)
+        p.scale(1, blink)
+          p.fill(...innerColor))
       p.noStroke()
       p.ellipse(0, 0, eyeSize, eyeSize)
       
-       p.fill(100)
+       // p.fill(100)
       p.noStroke()
-      p.fill(hue, 100, brightness + 20)
+      p.fill(...innerColor)
       p.ellipse(0, -eyeSize*.4, eyeSize*.6, eyeSize*.6)
+        
+        p.pop()
       }
       
       // Only two eyes....?
-      p.push()
-      p.translate(eyeWidth, -eyeHeight)
-      p.scale(1, bounce)
-      drawEye()
-      p.pop()
+   
       
-       p.push()
+      drawEye({x: eyeWidth, y: -eyeHeight, eyeSize:eyeSize })
+     drawEye({x: -eyeWidth, y: -eyeHeight, eyeSize:eyeSize })
       
-      p.translate(-eyeWidth, -eyeHeight)
-      p.scale(1, bounce)
-      drawEye()
-      p.pop()
       
       
       p.pop()
