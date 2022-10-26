ca3f5077f7e93dae4c0fb63ebf7a9cd38dee114a
Glitch (glitch-hello-website) <none>
2022-10-26 16:32:25 +0000

    🎀🙅 Checkpoint ./bots.js:270926/132

--- bots.js
+++ bots.js
@@ -1,7 +1,8 @@
 const BOTS = {
   
   myBot: {
-    description: [],
+    title: "Cocoa-and-Therapy Bot",
+    description: ["a bot for suggesting hot drinks and listening to your problems"],
     grammar: {
       toppings: ["caramel sauce", "mini marshmallows", "a candy cane", "sprinkles", "whipped cream", "vegan whip", "marshmallow fluff", "grated nutmeg"],
       milk: ["oatmilk", "soy", "whole milk", "skim", "almond milk"],
