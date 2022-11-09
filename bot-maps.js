df180ad8b28f8639e9b8e199defd54f8f1faed1b
Glitch (glitch-hello-website) <none>
2022-11-09 21:53:50 +0000

    🌾🍰 Checkpoint ./bot-maps.js:49226102/1682

--- bot-maps.js
+++ bot-maps.js
@@ -208,26 +208,25 @@ const BOT_MAPS = {
         ],
       },
        makeFood: {
-        onEnterSay: "Okay I got ya",
+        onEnterSay: "mmh amazing choice",
         exits: [
-          "wait:5 ->listen0",
+          "wait:5 ->tellPoem",
           "'something else' ->makeDrink 'How about something different then?'",
           "* ->listen '*SLURP*'",
         ],
       },
-
-      listen0: {
-        onEnterSay: "#askAboutUser#",
+      tellPoem: {
+        onEnterSay: "mmmh amazing ,  ",
         exits: [
-          "wait:5 ->origin 'Quiet time is good too'",
-          "'*' ->origin '#sympathy#'",
+          "wait:5 ->tellPoem",
+          "'something else' ->makeDrink 'How about something different then?'",
+          "* ->listen '*SLURP*'",
         ],
-      },
-      listen1: {
-        onEnterSay: ["#sympathy#", "#askAboutUser#"],
-      },
+      }
+      
 
-      exits: ["'*' -> '#sympathy#'"],
+      
+      
     },
     grammar: {
       askAboutUser: [
@@ -235,41 +234,13 @@ const BOT_MAPS = {
         "What's on your mind?",
         "How is this week going?",
       ],
-      listen: [
-        "mmhmm",
-        "tell me about it",
-        "tell me more?",
-        "does that happen often?",
-      ],
-      sympathy: [
-        "that sounds #difficult#",
-        "you've been through a lot",
-        "it sounds like you are trying very hard",
-      ],
-      difficult: ["challenging", "hard", "like a tough time"],
-      toppings: [
-        "caramel sauce",
-        "mini marshmallows",
-        "a candy cane",
-        "sprinkles",
-        "whipped cream",
-        "vegan whip",
-        "marshmallow fluff",
-        "grated nutmeg",
-      ],
-      milk: ["oatmilk", "soy", "whole milk", "skim", "almond milk"],
-      coffeeType: [
-        "latte",
-        "chai",
-        "espresso",
-        "frappe",
-        "mocha",
-        "hot chocolate",
-      ],
-      food: [
-        "pasta",
-        "pizza",
-        "rice",
+       
+      pasta: [
+        "spaghetti",
+        "fettucine",
+        "farfalle",
+        "penne",
+        "ravioli"
        
       ]
     },
