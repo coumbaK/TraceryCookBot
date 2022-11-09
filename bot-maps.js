538e7c16cee5f53cab123469fd3c41c02aa67196
Glitch (glitch-hello-website) <none>
2022-11-09 21:41:46 +0000

    💅⛩ Checkpoint ./bot-maps.js:49226102/13

--- bot-maps.js
+++ bot-maps.js
@@ -192,7 +192,7 @@ const BOT_MAPS = {
         onEnterSay:
           "I will make you food for dinner and tell you poems. What are you craving tonight?",
         exits: [
-          "#food# ->makeFood",
+          "'pasta' ->makeFood",
           "#coffeeType#->makeDrink",
           
         ],
