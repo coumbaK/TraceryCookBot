d12c51b5b4f9fa7c96189c325a9fe45e6c830a43
Glitch (glitch-hello-website) <none>
2022-11-09 04:56:43 +0000

    🎾🚵 Checkpoint ./bot-maps.js:49226102/2891

--- bot-maps.js
+++ bot-maps.js
@@ -181,4 +181,97 @@ const BOT_MAPS = {
       ],
     },
   },
+  Homiebot: {
+    title: "Food and Poetry Bot",
+    description: [
+      "a bot for suggesting food and composing poems",
+    ],
+
+    states: {
+      origin: {
+        onEnterSay:
+          "I will make you food for dinner and tell you poems. What are you craving tonight?",
+        exits: [
+          "'##' ->makeFood",
+          "'drink' ->makeDrink",
+          
+        ],
+      },
+      
+
+      makeDrink: {
+        onEnterSay: "I'll make you a #coffeeType#.",
+        exits: [
+          "wait:5 ->listen0",
+          "'something else' ->makeDrink 'How about something different then?'",
+          "* ->listen '*SLURP*'",
+        ],
+      },
+       makeFood: {
+        onEnterSay: "Okay I got ya",
+        exits: [
+          "wait:5 ->listen0",
+          "'something else' ->makeDrink 'How about something different then?'",
+          "* ->listen '*SLURP*'",
+        ],
+      },
+
+      listen0: {
+        onEnterSay: "#askAboutUser#",
+        exits: [
+          "wait:5 ->origin 'Quiet time is good too'",
+          "'*' ->origin '#sympathy#'",
+        ],
+      },
+      listen1: {
+        onEnterSay: ["#sympathy#", "#askAboutUser#"],
+      },
+
+      exits: ["'*' -> '#sympathy#'"],
+    },
+    grammar: {
+      askAboutUser: [
+        "How was your day?",
+        "What's on your mind?",
+        "How is this week going?",
+      ],
+      listen: [
+        "mmhmm",
+        "tell me about it",
+        "tell me more?",
+        "does that happen often?",
+      ],
+      sympathy: [
+        "that sounds #difficult#",
+        "you've been through a lot",
+        "it sounds like you are trying very hard",
+      ],
+      difficult: ["challenging", "hard", "like a tough time"],
+      toppings: [
+        "caramel sauce",
+        "mini marshmallows",
+        "a candy cane",
+        "sprinkles",
+        "whipped cream",
+        "vegan whip",
+        "marshmallow fluff",
+        "grated nutmeg",
+      ],
+      milk: ["oatmilk", "soy", "whole milk", "skim", "almond milk"],
+      coffeeType: [
+        "latte",
+        "chai",
+        "espresso",
+        "frappe",
+        "mocha",
+        "hot chocolate",
+      ],
+      food: [
+        "pasta",
+        "pizza",
+        "rice",
+       
+      ]
+    },
+  }
 };
